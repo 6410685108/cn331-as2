eb97998f03fa0c0580077bbca847ded58b15ad45
Techin Bunphuttharak <6410685108@cn330>
2023-09-23 20:56:06 +0700

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticate:
+        return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'users/index.html')
