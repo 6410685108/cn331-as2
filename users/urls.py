cfbd48fb0ac81a50d6c1fda8a842b865055615ee
Techin Bunphuttharak <6410685108@cn330>
2023-09-28 20:58:37 +0700

    Initial commit

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('users', include('django.contrib.auth.urls'))
 ]
