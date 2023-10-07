2c9479aa712091de3a7526c207a528871f950521
Korawat Plainsuwan <6410685066@cn103>
2023-10-07 23:03:21 +0700

    last3

--- subjects/admin.py
+++ subjects/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import subject
+from users.models import ListRegSubject
 
-# Register your models here.
+class ListRegSubjectInline(admin.TabularInline):  # หรือใช้ StackedInline แทน
+    model = ListRegSubject
+    extra = 1  # กำหนดจำนวนช่องในแบบฟอร์ม
+
 class adminsubject(admin.ModelAdmin):
     list_display = ['subjectName', 'subjectID', 'remaining_class', 'max_class', 'subjectStatus']
     search_fields = ['subjectName']
     list_filter = ['subjectStatus']
-
+    inlines = [ListRegSubjectInline]
 
 admin.site.register(subject, adminsubject)
